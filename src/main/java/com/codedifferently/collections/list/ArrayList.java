d1ac60b047fb8671ee315bec4fcbb33889bd556e
Alberto Rodriguez <ararod9@gmail.com>
2021-01-27 19:37:59 -0500

    no changes

--- src/main/java/com/codedifferently/collections/list/ArrayList.java
+++ src/main/java/com/codedifferently/collections/list/ArrayList.java
@@ -1,11 +1,11 @@
 package com.codedifferently.collections.list;
 
-import com.codedifferently.collections.Collection;
+
 import com.codedifferently.collections.interfaces.List;
 
 import javax.lang.model.element.Element;
 
-public class ArrayList<E> extends Collection implements List {
+public class ArrayList<E> implements List {
 
     @Override
     public void contains(Object o) {
